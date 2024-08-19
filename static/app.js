19c7d4b6731ba60e09f5f3829adcbf0cde59557e
khj180610 <khj180610@kakao.com>
2024-08-19 16:54:15 +0900

    .

---
+++ static/app.js
@@ -0,0 +1,82 @@
+async function editMemo(event) {
+  const id = event.target.dataset.id;
+  //console.log(event.target.dataset.id);
+  const editInput = prompt("수정할 값을 입력하세요"); //값을 입력할수있음
+  const res = await fetch(`/memos/${id}`, {
+    method: "PUT", //값 수정
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      //통신은 문자열만 가능하다
+      id,
+      content: editInput,
+    }),
+  });
+  readMemo();
+}
+
+async function deleteMemo(event) {
+  const id = event.target.dataset.id;
+  const res = await fetch(`/memos/${id}`, {
+    method: "DELETE", //값 삭제
+  });
+  readMemo();
+}
+
+function displayMemo(memo) {
+  const ul = document.querySelector("#memo-ul");
+
+  const li = document.createElement("li");
+  li.innerText = `[id:${memo.id}] ${memo.content}`;
+
+  const editBtn = document.createElement("button");
+  editBtn.innerText = "수정하기";
+  editBtn.addEventListener("click", editMemo);
+  editBtn.dataset.id = memo.id; //메모아이디 버튼아이디로 설정
+
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "삭제";
+  delBtn.addEventListener("click", deleteMemo);
+  delBtn.dataset.id = memo.id;
+
+  li.appendChild(editBtn);
+  li.appendChild(delBtn);
+  ul.appendChild(li);
+}
+
+async function readMemo() {
+  const res = await fetch("/memos");
+  const jsonRes = await res.json();
+  const ul = document.querySelector("#memo-ul");
+  ul.innerHTML = "";
+  jsonRes.forEach(displayMemo);
+}
+
+async function createMemo(value) {
+  const res = await fetch("/memos", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      //통신은 문자열만 가능하다
+      id: new Date().getTime(),
+      content: value,
+    }),
+  });
+  readMemo();
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = document.querySelector("#memo-input");
+  createMemo(input.value);
+  input.value = "";
+}
+
+const form = document.querySelector("#memo-form");
+//console.log(form);
+form.addEventListener("submit", handleSubmit);
+
+readMemo();
